92e681f9f54ece6f11d7b1a1cc29afb2c7aaf72b
Andrew Martin <andrew.thaddeus@gmail.com>
2019-07-22 09:58:38 -0400

    Expose more traffic log fields to users

--- src/Panos/Syslog/Traffic.hs
+++ src/Panos/Syslog/Traffic.hs
@@ -7,9 +7,15 @@
 {-# language GeneralizedNewtypeDeriving #-}
 
 module Panos.Syslog.Traffic
-  ( deviceName
-  , packetsReceived
+  ( bytes
   , bytesReceived
+  , bytesSent
+  , deviceName
+  , packets
+  , packetsReceived
+  , packetsSent
+  , sourceUser
+  , destinationUser
   ) where
 
 import Data.Bytes.Types (Bytes(..))
@@ -21,9 +27,33 @@ deviceName :: Traffic -> Bytes
 deviceName (Traffic{deviceName=Bounds off len,message=msg}) =
   Bytes{offset=fromIntegral off,length=fromIntegral len,array=msg}
 
+sourceUser :: Traffic -> Maybe Bytes
+sourceUser (Traffic{sourceUser=Bounds off len,message=msg}) =
+  if len > 0
+    then Just Bytes{offset=fromIntegral off,length=fromIntegral len,array=msg}
+    else Nothing
+
+destinationUser :: Traffic -> Maybe Bytes
+destinationUser (Traffic{destinationUser=Bounds off len,message=msg}) =
+  if len > 0
+    then Just Bytes{offset=fromIntegral off,length=fromIntegral len,array=msg}
+    else Nothing
+
 packetsReceived :: Traffic -> Word64
 packetsReceived = U.packetsReceived
 
+packetsSent :: Traffic -> Word64
+packetsSent = U.packetsSent
+
+packets :: Traffic -> Word64
+packets = U.packets
+
 bytesReceived :: Traffic -> Word64
 bytesReceived = U.bytesReceived
+
+bytesSent :: Traffic -> Word64
+bytesSent = U.bytesSent
+
+bytes :: Traffic -> Word64
+bytes = U.bytes
 
