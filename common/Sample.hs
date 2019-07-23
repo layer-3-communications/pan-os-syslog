a9ae3b2ab902624166da221958cc74a3e136eb15
Andrew Martin <andrew.thaddeus@gmail.com>
2019-07-23 17:03:51 -0400

    add a sample url threat log

--- common/Sample.hs
+++ common/Sample.hs
@@ -2,6 +2,7 @@
 
 module Sample
   ( traffic_8_1_A
+  , threat_8_1_A
   ) where
 
 import Data.Primitive (ByteArray)
@@ -23,4 +24,18 @@ traffic_8_1_A = pack $ concat
   , "0,5384825641,0x8000000000000000,10.0.0.0-10.255.255.255,"
   , "10.0.0.0-10.255.255.255,0,1,0,policy-deny,174,0,0,0,MY-VSYS-NAME,"
   , "MY-DEVICE-NAME,from-policy,,,0,,0,,N/A,0,0,0,0"
+  ]
+
+threat_8_1_A :: ByteArray
+threat_8_1_A = pack $ concat
+  [ "Jul 23 15:45:11 NY-APP-8.local 1,2019/07/23 15:45:11,"
+  , "028191718331,THREAT,url,2056,2019/07/23 15:45:12,192.0.2.240,"
+  , "192.0.2.243,192.0.2.54,192.0.2.13,MY-RULE-NAME,,,ssl,vsys79,"
+  , "src-zone,dst-zone,eth3/2.7,eth6/4,Example-Forwarding-Policy,"
+  , "2019/07/23 15:45:17,95963,2,59457,443,3984,443,0x400000,tcp,"
+  , "alert,\"www.example.com/\",(9999),"
+  , "the-category-name,informational,client-to-server,"
+  , "97089310,0xa000000000000000,10.0.0.0-10.255.255.255,United States,"
+  , "0,,0,,,0,,,,,,,,0,225,0,0,0,,NY-APP-8,,,,,0,,0,,N/A,unknown,"
+  , "AppThreat-1-6,0x0,0,3176142484,"
   ]
